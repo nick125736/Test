f0fb75c8e5f73406b6d2f51162ccde99fc9ef8de
nick125736 <110b26379@mailst.cjcu.edu.tw>
2023-03-29 14:26:35 +0800

    picture

--- app/src/main/java/com/example/test/ResultActivity.java
+++ app/src/main/java/com/example/test/ResultActivity.java
@@ -23,15 +23,21 @@ public class ResultActivity extends AppCompatActivity {
         double ww = bundle.getDouble("keyweight",50.0);
         double bmi = ww / ((hh / 100.0) * (hh /100.0));
 
+        ImageView showpic = findViewById(R.id.ivShow);
+
 
         if(bmi<18.5) {
             Mes = "體重過輕";
+            showpic.setImageResource(R.mipmap.s1);
         } else if (bmi >=18.5 && bmi < 24) {
             Mes = "體重正常";
+            showpic.setImageResource(R.mipmap.s2);
         } else if (bmi >=24 && bmi < 27) {
             Mes = "體重過重";
+            showpic.setImageResource(R.mipmap.s3);
         } else {
             Mes = "太胖了";
+            showpic.setImageResource(R.mipmap.s4);
         }
 
         TextView showbmi = findViewById(R.id.tvShowBMI);
@@ -39,8 +45,8 @@ public class ResultActivity extends AppCompatActivity {
     }
 
     public void Gohome(View view) {
-        Intent intent  = new Intent(this, MainActivity.class);
-        startActivity(intent);
-
+//        Intent intent  = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+        finish();
     }
 }
