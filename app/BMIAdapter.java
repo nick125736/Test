bb0a17457928b074ce9a932511033c09560d8d8e
n.k125736 <110b26379@mailst.cjcu.edu.tw>
2023-05-10 14:26:50 +0800

    recycleview-1

---
+++ app/BMIAdapter.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class BMIAdapter{
+    private ArrayList<BMIdata> bmidataList;
+
+    public BMIAdapter(ArrayList<BMIdata> bmidataList) {
+        this.bmidataList = bmidataList;
+    }
+}
---
+++ app/BMIdata.java
@@ -0,0 +1,29 @@
+public class BMIdata {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    String name;
+    double height;
+    double weight;
+}
